165f6c038bedeb38b3d18066dae24ab234abb0e4
PhucLoc <phuclocub@gmail.com>
2024-01-27 10:28:28 +0700

    done front end

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from "./Home";
-import SideBar from "./component/SideBar";
-import ProfileTab from "./user-comp/ProfileTab";
 class App extends Component{
   render(){
       return(
-          <div className="d-flex flex-row mb-3" style={{backgroundColor:"#f0f4fc"}}>
-              <SideBar/>
-              <ProfileTab/>
-          </div>
+          <Router>
+            <Routes>
+              <Route path='/crypto-checker' exact={true} element={<Home/>}/>
+            </Routes>
+          </Router>
       )
   }
 }
