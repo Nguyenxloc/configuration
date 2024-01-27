ce154c8eea39d2e7e93c02528c25a7653ed0195d
PhucLoc <phuclocub@gmail.com>
2024-01-27 10:19:31 +0700

    done front end

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from "./Home";
+import SideBar from "./component/SideBar";
+import ProfileTab from "./user-comp/ProfileTab";
 class App extends Component{
   render(){
       return(
-          <Router>
-            <Routes>
-              <Route path='/' exact={true} element={<Home/>}/>
-            </Routes>
-          </Router>
+          <div className="d-flex flex-row mb-3" style={{backgroundColor:"#f0f4fc"}}>
+              <SideBar/>
+              <ProfileTab/>
+          </div>
       )
   }
 }
