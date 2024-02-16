65f66bd3630b05a133494b378f2ee25437cebacb
PhucLoc <phuclocub@gmail.com>
2024-02-17 01:19:57 +0700

    done requirement

--- src/pages/WalletPage.js
+++ src/pages/WalletPage.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import BackButton from "../component/BackButton";
-import { ethers} from "ethers";
+import { ethers } from "ethers";
+import { Provider, types } from "zksync-ethers";
 import EthereumAddress from 'ethereum-address';
 import { useNavigate, useParams } from 'react-router-dom';
 import { ColorRing } from 'react-loader-spinner';
-import { Provider, Wallet, getDefaultProvider } from 'zksync';
 function WalletPage(props) {
     const emptyItem = {
         id: '',
@@ -14,18 +14,23 @@ function WalletPage(props) {
     const chainID = useParams().chainID;
     const id = useParams().id;
     const [lstCrypto, setCryptos] = useState([]);
-    console.log("state: "+JSON.stringify(lstCrypto));
+    console.log("state: " + JSON.stringify(lstCrypto));
     const [item, setItem] = useState('');
     const navigate = useNavigate();
     let lstEth = null;
     let total = 0;
+
     const processedID = (listAddress) => {
         return JSON.parse(listAddress);
     }
+
     const arr = processedID(id);
     useEffect(() => {
-        if (lstCrypto.length <=4 ) {
+        if (lstCrypto.length <= 4) {
+            if(chainID==="chainid324")
             collectCrypto324(arr);
+            if(chainID==="chainid1")
+            collectCrypto(arr);
         }
     }, [id, lstCrypto]);
 
@@ -33,7 +38,7 @@ function WalletPage(props) {
         return EthereumAddress.isAddress(address);
     }
 
-    if (lstCrypto !== undefined) {
+    if (lstCrypto !== undefined&&chainID==="chainid1") {
         lstEth = lstCrypto.map(crypto => {
             total += 2272 * crypto.amount;
             return <tr key={crypto.id}>
@@ -43,43 +48,62 @@ function WalletPage(props) {
                 <td >$2,272</td>
                 <td >{crypto.amount}</td>
                 <td >$ {2272 * crypto.amount}</td>
-                </tr>
+            </tr>
         });
     }
-    
+
+    if (lstCrypto !== undefined&&chainID==="chainid324") {
+        lstEth = lstCrypto.map(crypto => {
+            total += 2272 * crypto.amount;
+            return <tr key={crypto.id}>
+                <td >#</td>
+                <td >{crypto.id}</td>
+                <td >ETH-zksync</td>
+                <td >$2,272</td>
+                <td >{crypto.amount}</td>
+                <td >$ {2272 * crypto.amount}</td>
+            </tr>
+        });
+    }
+
+    function convertBigIntToString(obj) {
+        if (typeof obj === 'bigint') {
+            return obj.toString();
+        } else if (typeof obj === 'object' && obj !== null) {
+            for (let key in obj) {
+                obj[key] = convertBigIntToString(obj[key]);
+            }
+        }
+        return obj;
+    }
     async function collectCrypto324(addresses) {
         try {
-            // Create a provider for the chainId324 network
-            const provider = getDefaultProvider('mainnet', 'https://mainnet.era.zksync.io');
-            const zkSyncProvider =  new Provider("https://mainnet.era.zksync.io");
+            // Create a provider for the zkSync network
+            const provider = Provider.getDefaultProvider(types.Network.Mainnet);
             const updatedCryptos = [];
             // Iterate over each address in the array
             for (const address of addresses) {
-                // Fetch balance for the current address
-                const accountState = await zkSyncProvider.getState(address);
-                let balance = accountState.accountState.committed.balances.eth;
-                // Create a new item with address and balance
-                let newItem = { id: address, balance};
-                // Add the new item to the updatedCryptos array
-                updatedCryptos.push(newItem);
+                    const balance = await provider.getBalance(address);
+                    let newItem = { id: address, amount: parseFloat(ethers.formatEther(balance)) };
+                    // Add the new item to the updatedCryptos array
+                    updatedCryptos.push(newItem);
             }
-            // Update the state with the updatedCryptos array
             setCryptos(updatedCryptos);
         } catch (error) {
             console.error("Error:", error);
         }
     }
-    
+
     async function collectCrypto(addresses) {
         try {
-            const provider = new ethers.providers.JsonRpcBatchProvider('https://eth.nodeconnect.org');
+            const provider = new ethers.JsonRpcProvider('https://eth.nodeconnect.org');
             const updatedCryptos = [];
             // Iterate over each address in the array
             for (const address of addresses) {
                 // Fetch balance for the current address
                 let balance = await provider.getBalance(address);
                 // Create a new item with address and balance
-                let newItem = { id: address, amount: parseFloat(ethers.utils.formatEther(balance)) };
+                let newItem = { id: address, amount: parseFloat(ethers.formatEther(balance)) };
                 // Add the new item to the updatedCryptos array
                 updatedCryptos.push(newItem);
             }
@@ -108,7 +132,7 @@ function WalletPage(props) {
     return (
         <div className="row g-" style={{ backgroundColor: "#fffcfc" }}>
             <div className="col-md-4 mt-2">
-                <BackButton url={"/crypto-checker/wallet"}/>
+                <BackButton url={"/crypto-checker/wallet"} />
             </div>
             <hr />
             <div className="col-md-12">
@@ -128,7 +152,7 @@ function WalletPage(props) {
                             </tr>
                         </thead>
                         <tbody>
-                            {lstCrypto.length >0 ? lstEth : <ColorRing/>}
+                            {lstCrypto.length > 0 ? lstEth : <ColorRing />}
                             <tr>
                                 <td ></td>
                                 <td ></td>
@@ -137,7 +161,7 @@ function WalletPage(props) {
                                 <td ></td>
                                 <td ><strong>Total: ${total}</strong></td>
                             </tr>
-                        </tbody> 
+                        </tbody>
                     </table>
                 </div>
             </div>
