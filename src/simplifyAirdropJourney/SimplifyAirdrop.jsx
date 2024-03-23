f9d15bfa51255439002a8c10891200f626b9b9f6
nguyenxloc <phuclocub@gmail.com>
2024-03-24 03:32:50 +0700

    make subcription tab

--- src/simplifyAirdropJourney/SimplifyAirdrop.jsx
+++ src/simplifyAirdropJourney/SimplifyAirdrop.jsx
@@ -1,22 +1,100 @@
 import React from "react";
 
-export default function SimplifyAirdrop(){
-    return(
-      <div className="bg-white w-[1000px]"> 
-        <div>
-        <h2 className="font-medium text-[48px] text-center">Simplify your airdrop</h2>
-        <h2 className="font-medium text-[48px] text-center">journey: AutoAir AI,</h2>
-        <h2 className="font-medium text-[48px] text-center">One-click farming solution</h2>
-        <p className="text-gray9 font-medium text-[20px] text-center">Simple Pricing. Exceptional Benefits.</p>
-        </div> 
-        <div className="flex">
-            <div className="w-[581px] h-[352px]" style={{border:"red solid 1px"}}>
-                <div className="w-[320px] h-[52px] bg-gray10"></div>
+export default function SimplifyAirdrop() {
+    return (
+        <div className="bg-white w-[1000px]">
+            <div>
+                <h2 className="font-medium text-[48px] text-center">Simplify your airdrop</h2>
+                <h2 className="font-medium text-[48px] text-center">journey: AutoAir AI,</h2>
+                <h2 className="font-medium text-[48px] text-center">One-click farming solution</h2>
+                <p className="text-gray9 font-medium text-[20px] text-center">Simple Pricing. Exceptional Benefits.</p>
             </div>
-            <div className="w-[581px] h-[352px]" style={{border: "red solid 1px"}}>
-            <div className="w-[320px] h-[52px] bg-black"></div>
+            <div className="flex">
+                <div className="w-[550px] h-[552px]">
+                    <div className="">
+                        <div className="w-[320px] h-[52px] bg-gray10 rounded-l-full flex font-medium text-[20px] ms-[180px]">
+                            <p className="ms-[80px] mt-[10px]">Standard Tier</p>
+                            <div className="mt-[10px] ms-[10px] text-[16px] border border-black rounded-full p-[4px] h-[30px]">
+                                <p>Free</p>
+                            </div>
+                        </div>
+                        <div className="flex justify-end me-[32px]">
+                            <p className="font-medium mt-[15px]">Basic Access: Default Script Utilization</p>
+                            <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        </div>
+                        <div className="flex justify-end me-[32px]">
+                            <p className="font-medium mt-[15px]">Execute tasks on 1 dApp</p>
+                            <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        </div>
+                        <div className="flex justify-end me-[32px]">
+                            <p className="font-medium mt-[15px]">Advanced AI On-Chain Analysis</p>
+                            <img className="" src="hhttps://autoair.xyz/icon/x_v2.svg" alt="" />
+                        </div>
+                        <div className="flex justify-end me-[32px]">
+                            <p className="font-medium mt-[15px]">Algorithmic Randomization of Parameters</p>
+                            <img className="" src="https://autoair.xyz/icon/x_v2.svg" alt="" />
+                        </div>
+                        <div className="flex justify-end me-[32px]">
+                            <p className="font-medium mt-[15px]">Automated Task Execution: Weekly/Monthly Randomize</p>
+                            <img className="" src="https://autoair.xyz/icon/x_v2.svg" alt="" />
+                        </div>
+                        <div className="flex justify-end me-[32px]">
+                            <p className="font-medium mt-[15px]">New Strategy Module Activation</p>
+                            <img className="" src="https://autoair.xyz/icon/x_v2.svg" alt="" />
+                        </div>
+                        <div className="flex justify-end me-[32px]">
+                            <p className="font-medium mt-[15px]">Commission Sharing</p>
+                            <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        </div>
+                        <div className="flex justify-end me-[32px]">
+                            <p className="font-medium mt-[15px] text-end">Referral Sharing</p>
+                            <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="w-[550px] h-[552px]">
+                    <div className="">
+                        <div className="w-[320px] h-[52px] bg-black rounded-r-full flex text-white font-medium text-[20px] ">
+                            <p className="ms-[80px] mt-[10px]">Premium Tier</p>
+                            <div className="mt-[10px] ms-[10px] text-[16px] border rounded-full p-[4px] h-[30px]">
+                                <p>Beta free</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex ms-[32px]">
+                        <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        <p className="font-medium mt-[15px]">Basic Access: Default Script Utilization</p>
+                    </div>
+                    <div className="flex ms-[32px]">
+                        <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        <p className="font-medium mt-[15px]">Execute tasks on 1 dApp</p>
+                    </div>
+                    <div className="flex ms-[32px]">
+                        <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        <p className="font-medium mt-[15px]">Advanced AI On-Chain Analysis</p>
+                    </div>
+                    <div className="flex ms-[32px]">
+                        <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        <p className="font-medium mt-[15px]">Algorithmic Randomization of Parameters</p>
+                    </div>
+                    <div className="flex ms-[32px]">
+                        <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        <p className="font-medium mt-[15px]">Automated Task Execution: Weekly/Monthly Randomize</p>
+                    </div>
+                    <div className="flex ms-[32px]">
+                        <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        <p className="font-medium mt-[15px]">New Strategy Module Activation</p>
+                    </div>
+                    <div className="flex ms-[32px]">
+                        <img className="" src="https://autoair.xyz/icon/x_v2.svg" alt="" />
+                        <p className="font-medium mt-[15px]">Commission Sharing</p>
+                    </div>
+                    <div className="flex ms-[32px]">
+                        <img className="" src="https://autoair.xyz/icon/check_v2.svg" alt="" />
+                        <p className="font-medium mt-[15px] text-end">Referral Sharing</p>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>  
     );
 }
