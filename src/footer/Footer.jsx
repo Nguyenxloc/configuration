21db6d94d4a974ef5f5366abbd61e958d123dc5d
nguyenxloc <phuclocub@gmail.com>
2024-03-26 15:54:26 +0700

    done responsive 2.5/5

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -15,11 +15,11 @@ export default function Footer() {
             <div className="flex justify-center mt-[90px]">
                 <MeetAAITeleBtn />
             </div>
-            <div className="flex mt-[200px] ms-[30px] gap-[60px]" style={{border:"1px solid red"}}>
-                <div className="w-4/12 h-fit" style={{border:"solid red 1px"}}>
+            <div className="flex mt-[200px] ms-[30px] gap-[150px]" style={{border:"1px solid red"}}>
+                <div className="w-4/12 h-fit mt-[160px]" style={{border:"solid red 1px"}}>
                     <div>
                     </div>
-                    <p className="font-medium">Legal</p>
+                    <p className="font-bold text-[#555555]">Legal</p>
                     <div className="flex gap-[48px]">
                         <a className="text-[20px] text-medium text-white no-underline" href="">Term and service</a>
                         <a className="text-[20px] text-medium text-white no-underline" href="">Privacy policy</a>
@@ -32,13 +32,13 @@ export default function Footer() {
                         <p className="text-[32px] font-medium text-gray14 leading-[40px] ms-[100px] mt-[40px]">Back to top</p>
                     </div>
                 </div>
-                <div className="w-3/12 h-[232px]" style={{ border: "1px solid red" }}>
-                    <p className="text-right text-medium">About us</p>
+                <div className="w-3/12 h-fit mt-[225px]" style={{ border: "1px solid red" }}>
+                    <p className="text-right font-bold text-[#555555]">About us</p>
                     <div className="flex flex-row-reverse gap-[48px]">
                         <a className="text-[20px] text-white no-underline" href="">Blog</a>
                         <a className="text-[20px] text-white no-underline" href="">GitBook</a>
                     </div>
-                    <p className="text-right text-medium">Social</p>
+                    <p className="text-right font-bold text-[#555555]">Social</p>
                     <div className="flex flex-row-reverse gap-[48px]">
                         <a className="text-[20px] text-white no-underline" href="">Twitter X</a>
                         <a className="text-[20px] text-white no-underline" href="">Telegram</a>
